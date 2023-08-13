121cbcda0e10e4fffd03187a0b61c390048bef9a
GabrielReynolds <103075616@student.swin.edu.au>
2023-08-13 18:23:47 +1000

    Wiped main
    Yes I know this is bad practice but there was nothing in here and it was missing Capacitor, so I'm giving it a reset

--- src/components/Welcome/Welcome_Component.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import "./style.css";
-</script>
-
-<div class="welcome">
-    Welcome to <br />
-    <img src="/grouptag-logo.png" alt="Grouptag Logo" />
-</div>
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h2>Welcome to</h2>
-<h1>Grouptag</h1>
-<a href="/login">Login</a>
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
