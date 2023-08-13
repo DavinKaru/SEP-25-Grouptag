32ba1f70f900a1ce552cf678602faa0e24be1a15
GabrielReynolds <103075616@student.swin.edu.au>
2023-08-13 18:26:43 +1000

    Added back some missing files

---
+++ src/components/Welcome/Welcome_Component.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "./style.css";
+</script>
+
+<div class="welcome">
+    Welcome to <br />
+    <img src="/grouptag-logo.png" alt="Grouptag Logo" />
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "./style.css";
+    import SignUpComponent from "../../components/SignUp/SignUp_Component.svelte";
+    import WelcomeComponent from "../../components/Welcome/Welcome_Component.svelte";
+</script>
+
+<body>
+    <div class="box">
+        <WelcomeComponent />
+        <SignUpComponent />
+    </div>
+</body>
