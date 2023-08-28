b338019c08daab3b63f3d76cca415b0f66042d8e
andrewzw <yapzhewei2002@gmail.com>
2023-08-28 15:18:04 +1000

    restructured the components and fixed bugs

--- src/components/Login/Login_Component.svelte
+++ src/components/Login/Login_Component.svelte
@@ -1,6 +1,4 @@
-<script>
-	import './style.css';
-</script>
+
 
 <form>
 	<div style="margin-top: 42px;">
@@ -32,3 +30,70 @@
 
 	<a href="/" class="forgot-password">I forgot my password</a>
 </form>
+
+<style>
+	.input-field {
+    height: 60px;
+    width: 100%;
+    border-radius: 10px;
+    padding: 0 20px;
+    font-size: 15px;
+    background-color: #f4fcff;
+    border: none;
+    margin: 8px auto;
+    text-align: left;
+    box-sizing: border-box;
+}
+
+.login-button {
+    height: 60px;
+    width: 257px;
+    border-radius: 10px;
+    font-size: 20px;
+    font-weight: bold;
+    background-color: #44c7f7;
+    color: white;
+    border: none;
+    cursor: pointer;
+    margin-top: 30px;
+    margin-bottom: 8px;
+}
+
+.forgot-password {
+    display: block;
+    font-size: 10px;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    color: #e87e7e;
+    margin-bottom: 30px;
+}
+
+form>div>div {
+    margin: auto;
+    width: 80%;
+    text-align: left;
+}
+
+label {
+    color: #f4fcff;
+    font-size: 15px;
+    font-weight: bold;
+}
+
+@media screen and (max-height: 667px) {
+    .box {
+        height: calc(100vh - 250px);
+    }
+
+    .forgot-password {
+        padding: 0px auto;
+        margin-bottom: 0px;
+    }
+
+    .input-field {
+        margin: auto;
+
+    }
+}
+</style>
--- src/components/Welcome/Welcome_Component.svelte
+++ src/components/Welcome/Welcome_Component.svelte
@@ -1,9 +1,25 @@
-<script>
-	import './style.css';
-</script>
-
 <div class="welcome">
 	Welcome to
 	<br />
 	<img src="/grouptag-logo.png" alt="Grouptag Logo" />
 </div>
+
+<style>
+	.welcome {
+		flex-grow: 1;
+		color: #ffffff;
+		text-align: center;
+		font-size: 29px;
+		font-weight: bold;
+		font-family: 'Lincoln Road', Arial, Helvetica, sans-serif;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.welcome > img {
+		width: 266px;
+		height: auto;
+	}
+</style>
