408eaa02bfd7427da69a75193977543aad11e87c
andrewzw <yapzhewei2002@gmail.com>
2023-08-30 14:36:19 +1000

    added login validation

--- src/components/Login/Login_Component.svelte
+++ src/components/Login/Login_Component.svelte
@@ -1,35 +1,80 @@
-<form>
+<script>
+	let email = '';
+	let password = '';
+	let errorEmail = '';
+	let errorPassword = '';
+
+	const validateForm = () => {
+		let isValid = true;
+
+		// Validate email
+		const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+		if (!emailPattern.test(email)) {
+			errorEmail = 'Please enter a valid university email.';
+			isValid = false;
+		} else {
+			errorEmail = '';
+		}
+
+		// Validate password
+		if (password.length < 8) {
+			//subject to change
+			errorPassword = 'Password should be at least 8 characters long.';
+			isValid = false;
+		} else {
+			errorPassword = '';
+		}
+
+		if (isValid) {
+			// Send data to server for further validation after validating email and password
+		}
+	};
+</script>
+
+<form on:submit|preventDefault={validateForm}>
 	<div style="margin-top: 42px;">
-		<div>
+		<div style="margin-bottom: 31px;">
 			<label for="email">University Email:</label>
 			<input
+				bind:value={email}
 				type="email"
 				id="email"
 				name="email"
 				class="input-field"
 				placeholder="Enter your university email here"
-				style="margin-bottom: 31px;"
 			/>
+			<span class="error-msg">{errorEmail}</span>
 		</div>
 
 		<div>
 			<label for="password">Password:</label>
 			<input
+				bind:value={password}
 				type="password"
 				id="password"
 				name="password"
 				class="input-field"
 				placeholder="Enter your password here"
 			/>
+			<span class="error-msg">{errorPassword}</span>
 		</div>
 	</div>
 
-	<input type="button" value="Login to your account" class="login-button" />
-
-	<a href="/" class="forgot-password">I forgot my password</a>
+	<input type="submit" value="Login to your account" class="login-button" />
 </form>
+<a href="/" class="forgot-password">I forgot my password</a>
 
 <style>
+	.error-msg {
+		color: #e87e7e;
+		font-size: 10px;
+		font-weight: bold;
+		text-align: center;
+		margin: 3px 0 0;
+		padding: 0px;
+		display: block;
+	}
+
 	.input-field {
 		height: 60px;
 		width: 100%;
@@ -38,7 +83,7 @@
 		font-size: 15px;
 		background-color: #f4fcff;
 		border: none;
-		margin: 8px auto;
+		margin: 0px auto;
 		text-align: left;
 		box-sizing: border-box;
 	}
