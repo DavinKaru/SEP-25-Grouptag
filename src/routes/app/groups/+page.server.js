8f85ab1ea2e3d22e266099ebc1e143e8b5bbbd94
andrewzw <yapzhewei2002@gmail.com>
2023-09-25 10:52:40 +1000

    Changed table names

--- src/routes/app/groups/+page.server.js
+++ src/routes/app/groups/+page.server.js
@@ -1,9 +1,9 @@
 import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-    const { data: Groups } = await supabase.from('Groups').select();
-    const { data: GroupUsers } = await supabase.from('GroupUsers').select();
-    const { data: Users } = await supabase.from('Users').select();
+    const { data: Groups } = await supabase.from('groups').select();
+    const { data: GroupUsers } = await supabase.from('groups_users').select();
+    const { data: Users } = await supabase.from('users').select();
     return {
         Groups: Groups ?? [],
         GroupUsers: GroupUsers ?? [],
