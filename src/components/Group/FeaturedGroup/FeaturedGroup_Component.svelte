a906300c8a33dd0bf2e6bbc65d61437bca99c635
GabrielReynolds <103075616@student.swin.edu.au>
2023-10-09 22:20:54 +1100

    Fixed Back Button
    Copied code for merge reasons

--- src/components/Group/FeaturedGroup/FeaturedGroup_Component.svelte
+++ src/components/Group/FeaturedGroup/FeaturedGroup_Component.svelte
@@ -1,188 +1,262 @@
-<!-- Groups Header Component-->
+<!-- Featured Group Component-->
 <script>
 	import TagIcon from '../../TagIcons/TagIcon_Component.svelte'; // Import the TagIcon component
-	export let data; // Receive data as a prop from the parent component
+	export let data; // Receive data
 	const { Groups, GroupUsers, Users } = data;
 	// Debug logs
-	console.log('Data received:', data);
+	console.log('FeaturedGroup Component Data received:', data);
 	// Fetch users using groupid
 	function getUsers(groupId) {
 		const groupUserIds = GroupUsers.filter((gu) => gu.group_id === groupId).map((gu) => gu.user_id);
 		const users = Users.filter((user) => groupUserIds.includes(user.user_id));
 		// Debug logs
-		console.log('Group User IDs:', groupUserIds);
-		console.log('Users:', users);
+		// console.log('Group User IDs:', groupUserIds);
+		// console.log('Users:', users);
 		return users;
 	}
+
+	const featuredGroups = Groups.slice(0, 3); // Get the first 3 featured groups
 </script>
 
-<div id="container">
-	{#each Groups as group}
-		<div id="details">
-			<img src={group.logo_url} alt="Featued Group Logo" id="group-img" />
-			<div id="content">
-				<h1 id="group-name">{group.name}</h1>
-				<p id="group-description">{group.description}</p>
-
-				<div id="tags">
-					{#each group.tags as tag}
-						<TagIcon text={tag.name} />
-					{/each}
-				</div>
-
-				<div id="members">
-					<h2 id="members-header">Members</h2>
-					<div id="members-icons">
-						{#each getUsers(group.group_id) as user, i}
-							<!-- Only show the first 6 users -->
-							{#if i < 6}
-								<span class="icon" style="background-image: url({user.image_url});" />
-							{/if}
-						{/each}
+<div class="carousel-container">
+	{#each featuredGroups as featuredGroup}
+		<div class="carousel-item">
+			<div class="container">
+				<div class="left-content">
+					<img src={featuredGroup.banner_url} alt="Featued Group Logo" id="group-img" />
+					<div id="members">
+						<h2 id="members-header">Members</h2>
+						<div id="members-icons">
+							{#each getUsers(featuredGroup.group_id) as user, i}
+								<!-- Only show the first 6 users -->
+								{#if i < 6}
+									<span class="icon" style="background-image: url({user.image_url});" />
+								{/if}
+							{/each}
+						</div>
 					</div>
 				</div>
+				<div class="right-content">
+					<div class="text">
+						<h1 id="group-name">{featuredGroup.name}</h1>
+						<p id="group-description">{featuredGroup.description}</p>
+					</div>
+					<div id="tags">
+						{#each featuredGroup.tags as tag}
+							<TagIcon text={tag.name} />
+						{/each}
+					</div>
 
-				<a href="default" id="join-button">Join Group</a>
+					<a href="default" id="join-button">Join Group</a>
+				</div>
 			</div>
 		</div>
 	{/each}
 </div>
 
 <style>
-	#container {
+	.carousel-container {
+		display: flex;
+		overflow-x: scroll;
+		scroll-snap-type: x mandatory;
+		webkit-overflow-scrolling: touch; /* smooth scrolling on iOS */
+	}
+
+	.carousel-item {
+		flex: 0 0 auto;
+		width: 100%;
+		margin-right: 2%;
+		scroll-snap-align: start;
+		scroll-snap-stop: always;
+	}
+	.container {
 		/* Colors */
 		background-color: rgba(255, 255, 255, 0.127);
 
 		/* Dimensions */
-		margin-top: 10px;
-		width: 10rem; /* Do not change plz! */
 		height: fit-content;
-		border-radius: 10px; /* Rounded corners on top left and right */
+		width: 100%;
+		border-radius: 10px;
+		margin: auto;
+
+		/* Flexbox layout */
+		display: flex;
+		flex-direction: column;
+		flex-wrap: nowrap;
+
+		flex-direction: row;
+		justify-content: center;
 	}
 
-	/* Phone Layout */
-	@media only screen and (min-width: 650px) {
+	.left-content {
+		flex-basis: 50vw;
+		padding: 5px;
+	}
 
-		#container {
-			/* Colors */
-			background-color: rgba(255, 255, 255, 0.127);
+	.right-content {
+		flex-basis: 50vw;
+		padding: 10px;
+		padding-bottom: 50px;
+		position: relative;
+	}
 
-			/* Dimensions */
-			margin-top: 10px;
-			width: 10rem; /* Do not change plz! */
-			height: fit-content;
-			border-radius: 10px; /* Rounded corners on top left and right */
+	#group-img {
+		width: 100%;
+		min-height: 93px;
+		border-radius: 10px;
+	}
 
-			display: flex;
-			flex-direction: column;
-			width: 55%;
+	#group-name {
+		font-size: 1.25rem;
+		color: white;
+	}
+
+	#group-description {
+		font-size: 0.5rem;
+	}
+
+	#tags {
+		margin-top: 6px;
+		display: flex;
+		gap: 5px;
+	}
+
+	#join-button {
+		display: inline-block;
+		padding: 0.3em 1.2em;
+		margin: 0.3em 0.3em 0.3em 0;
+		border-radius: 2em;
+		box-sizing: border-box;
+		text-decoration: none;
+		font-family: 'Roboto', sans-serif;
+		font-weight: 300;
+		color: #ffffff;
+		background-color: #3aa4d1;
+		text-align: center;
+		transition: all 0.2s;
+		position: absolute;
+		bottom: 10px;
+		right: 10px;
+	}
+
+	#join-button:hover {
+		background-color: #4095c6;
+	}
+
+	#members {
+		margin-left: 5px;
+		margin-top: 10px;
+	}
+
+	#members-header {
+		font-size: 0.8em;
+		color: white;
+	}
+
+	#members-icons {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+	}
+
+	.icon {
+		margin-top: 6px;
+		margin-bottom: 6px;
+		width: 6vw;
+		height: 6vw;
+		border-radius: 50%;
+		background-position: center; /* Center the background */
+		background-size: cover; /* Cover the entire area */
+		background-color: #ffffff;
+		border: 1px solid #f5f5f5;
+		display: inline-block;
+		margin-right: 1%; /* Gap between icons */
+	}
+
+	/* Responsive font sizes */
+	@media screen and (max-width: 1920px) {
+		.container {
+			width: 80%;
 		}
-
-		#group-img {
-			border-radius: 10px; /* Rounded corners on top left and right */
+		#group-name {
+			font-size: 1.8rem;
 		}
-
-		#tags {
-			display: flex;
-			height: fit-content;
-			gap: 5px;
+		#members-header {
+			font-size: 1.3rem;
 		}
-
-		#content {
-			margin-left: 10px;
-			margin-right: 10px;
-			margin-top: 4px;
-
-			display: flex;
-			flex-direction: column;
-			gap: 5px;
-		}
-
-		#join-button {
-			display: inline-block;
-			padding: 0.3em 1.2em;
-			margin-right: auto;
-			margin-bottom: 10px;
-			width: fit-content;
-			border-radius: 2em;
-			box-sizing: fit-content;
-			text-decoration: none;
-			font-family: 'Roboto', sans-serif;
-			font-weight: 300;
-			color: #ffffff;
-			background-color: #3aa4d1;
-			text-align: center;
-			transition: all 0.2s;
-		}
-
-		#join-button:hover {
-			background-color: #4095c6;
-		}
-
-		.icon {
-			margin-top: 6px;
-			margin-bottom: 6px;
-			width: 3vw;
-			height: 3vw;
-			border-radius: 50%;
-			background-position: center; /* Center the background */
-			background-size: cover; /* Cover the entire area */
-			background-color: #ffffff;
-			border: 1px solid #f5f5f5;
-			display: inline-block;
-			margin-right: 1%; /* Gap between icons */
-		}
-
-		@media only screen and (max-width: 650px) {
-			#group-img {
-				width: 100%;
-			}
-
-			#group-name {
-				font-size: 18px;
-			}
-
-			#group-description {
-				font-size: 10px;
-			}
-
-			#members-header {
-				font-size: 15px;
-			}
+		#group-description {
+			font-size: 1.1rem;
 		}
 	}
 
-	@media only screen and (min-width: 650px) {
-		#container {
+	@media screen and (max-width: 1280px) {
+		.container {
+			width: 75%;
+		}
+		#group-name {
+			font-size: 1.8rem;
+		}
+		#members-header {
+			font-size: 1.3rem;
+		}
+		#group-description {
+			font-size: 1.1rem;
+		}
+	}
+
+	@media screen and (max-width: 992px) {
+		.container {
 			width: 100%;
-			margin-left: auto;
-			margin-right: auto;
 		}
-
-		#details {
-			display: flex;
+		#group-name {
+			font-size: 1.6rem;
 		}
-
-		#group-img {
-			width: 50%;
-			margin-left: auto;
-			margin-right: auto;
+		#members-header {
+			font-size: 1.2rem;
 		}
+		#group-description {
+			font-size: 1rem;
+		}
+	}
 
-		#content {
-			display: flex;
+	@media screen and (max-width: 768px) {
+		.container {
+			width: 100%;
+		}
+		#group-name {
+			font-size: 1.4rem;
+		}
+		#members-header {
+			font-size: 1.1rem;
+		}
+		#group-description {
+			font-size: 0.9rem;
+		}
+	}
+
+	@media screen and (max-width: 576px) {
+		.container {
+			width: 100%;
 		}
 
 		#group-name {
-			font-size: 20px;
+			font-size: 1rem;
 		}
-
-		#group-description {
-			font-size: 12px;
-		}
-
 		#members-header {
-			font-size: 15px;
+			font-size: 0.8rem;
+		}
+		#group-description {
+			font-size: 0.5rem;
+		}
+
+		#members-icons {
+			flex-wrap: wrap;
+			column-gap: 2px;
+		}
+		.icon {
+			width: 6vw;
+			height: 6vw;
+			margin-top: 0px;
 		}
 	}
 </style>
