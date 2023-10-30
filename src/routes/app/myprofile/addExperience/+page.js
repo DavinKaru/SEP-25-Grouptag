1d87b463597ae94f3871a461602b66015946dff0
GabrielReynolds <103075616@student.swin.edu.au>
2023-10-30 22:16:34 +1100

    Fixed stuff

--- src/routes/app/myprofile/addExperience/+page.js
+++ src/routes/app/myprofile/addExperience/+page.js
@@ -1,12 +1,15 @@
 // @ts-nocheck
 //src/routes/app/myprofile/addExperience/+page.js
 
+import { supabase } from '../../../../supabaseClient.js';
+
 export async function load({ url, parent }) {
-    const { supabase, session } = await parent()
-    let user_id = url.searchParams.get('id') ?? '';
-    if (user_id == '') {
-        user_id = session.user.id
-    }
+
+    let user_id = 'f8fe9f2f-2ddb-4c64-945a-6f686a0d614f';
+    const myUserId = (await supabase.auth.getSession()).data.session?.user.id
+     if (myUserId != null){
+         user_id = myUserId;
+     }
 
     const { data: user } = await supabase.from('user_courses').select("users(*), university_courses (*), universities (*)").eq('user_id', user_id).single();
     const { data: Users } = await supabase.rpc('get_mutuals', { search_user_id: user_id });
