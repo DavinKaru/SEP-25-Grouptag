33a0d8922f5d7ab982ae6a829f191059d4a84eea
andrewzw <yapzhewei2002@gmail.com>
2023-11-01 23:04:34 +1100

    .

--- src/routes/app/create/group/+page.svelte
+++ src/routes/app/create/group/+page.svelte
@@ -6,8 +6,9 @@
 	import Dropzone from 'svelte-file-dropzone/Dropzone.svelte';
 	import toast, { Toaster } from 'svelte-french-toast';
 	import { supabase } from '$lib/supabaseClient';
+	export let data;
 
-	console.log('Autheticated user:', supabase.auth.user);
+	console.log('Create Post - Autheticated user:', supabase.auth.user);
 	//Insert data from the form into the database
 	let name;
 	let logo_url;
